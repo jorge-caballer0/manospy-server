feb822f6bd5b38de1f6e5cc28ce7b3dee3d37961
jorge-caballer0 <58111877+jorge-caballer0@users.noreply.github.com>
2026-01-21 11:15:55 -0300

    config.js

--- config/config.js
+++ config/config.js
@@ -1,29 +1,28 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: "postgres",
-    password: "tu_password_local",
-    database: "manospy_dev",
-    host: "127.0.0.1",
-    dialect: "postgres"
-  },
-  test: {
-    username: "postgres",
-    password: "tu_password_local",
-    database: "manospy_test",
-    host: "127.0.0.1",
-    dialect: "postgres"
-  },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
     }
+  },
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "manospy_dev",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "postgres",
+    password: "postgres",
+    database: "manospy_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   }
 };
-
