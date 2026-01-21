a1df8f883380ac6935447074b528ad75b3823150
jorge-caballer0 <58111877+jorge-caballer0@users.noreply.github.com>
2026-01-21 14:03:16 -0300

    reservationRoutes.js

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { getReservations, getMessages, addMessage } = require('../controllers/reservationController');
+const {
+  getReservations,
+  createReservation,
+  acceptReservation,
+  rejectReservation,
+  getMessages,
+  addMessage
+} = require('../controllers/reservationController');
 
 // Rutas de reservas
 router.get('/', getReservations);
+
+// Crear nueva reserva (cliente solicita servicio)
+router.post('/', createReservation);
+
+// Profesional acepta/rechaza reserva
+router.post('/:id/accept', acceptReservation);
+router.post('/:id/reject', rejectReservation);
+
+// Chat de mensajes dentro de una reserva
 router.get('/:id/messages', getMessages);
 router.post('/:id/messages', addMessage);
 
