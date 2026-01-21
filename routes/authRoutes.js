a1e2ab7d644dc5801130a52109d91038af5119cc
jorge-caballer0 <58111877+jorge-caballer0@users.noreply.github.com>
2026-01-21 08:32:35 -0300

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { registerClient, registerProfessional, login } = require('../controllers/authController');
 
-// Registro y login
-router.post('/register', register);
+// Registro
+router.post('/register/client', registerClient);
+router.post('/register/professional', registerProfessional);
+
+// Login
 router.post('/login', login);
 
 module.exports = router;
