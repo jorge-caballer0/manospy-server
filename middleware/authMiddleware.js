4e9b9d44f638dc18c738144ce8dc7e99ff3841eb
jorge-caballer0 <58111877+jorge-caballer0@users.noreply.github.com>
2026-01-22 13:13:55 -0300

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+// middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ message: "Token no proporcionado" });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret_manospy');
+    req.user = decoded; // aquí guardamos los datos del usuario en la request
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token inválido" });
+  }
+};
+
+module.exports = authMiddleware;
