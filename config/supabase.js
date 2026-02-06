9c48c6bb8430390b69433cacb99236be7db50bf4
jorge-caballer0 <58111877+jorge-caballer0@users.noreply.github.com>
2026-02-06 15:38:11 -0300

    Add files via upload

--- config/supabase.js
+++ config/supabase.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
+import 'dotenv/config';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = { supabase };
+export const supabase = createClient(supabaseUrl, supabaseKey);
